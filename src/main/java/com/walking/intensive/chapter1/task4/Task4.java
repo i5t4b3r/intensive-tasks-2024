0a558456e497d080fe06a310988ff4c2d5b17ce1
i5t4b3r <i5t4b3r@gmail.com>
2024-11-25 14:53:15 +0300

    end task 4

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -23,19 +23,42 @@ package com.walking.intensive.chapter1.task4;
  * <p><a href="https://github.com/KFalcon2022/intensive-tasks-2024/blob/master/README.md">Требования к оформлению</a>
  */
 public class Task4 {
+    static final String ENDLESS_SOLUTIONS = "Бесконечное множество решений.";
+    static final String ZERO_SOLUTIONS = "Количество решений: 0.";
+    static final String ONE_SOLUTIONS = "Количество решений: 1. Корень: ";
+    static final String TWO_SOLUTIONS = "Количество решений: 2. Корни: ";
+
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 0;
+        double a = 1;
         double b = 0;
         double c = 0;
 
         System.out.println(solveEquation(a, b, c));
-
     }
 
     static String solveEquation(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        if (a == 0) {
+            if (b == 0) {
+                return c == 0 ? ENDLESS_SOLUTIONS : ZERO_SOLUTIONS;
+            }
+            return ONE_SOLUTIONS + (-c / b);
+        }
+
+        double discriminant = b * b - 4 * a * c;
+
+        if (discriminant < 0) {
+            return ZERO_SOLUTIONS;
+        }
+
+        if (discriminant == 0) {
+            double root = -b / 2 * a;
+            return ONE_SOLUTIONS + (root == -0 ? 0 : root);
+        }
+
+        double firstRoot = (-b + Math.sqrt(discriminant)) / (2 * a);
+        double secondRoot = (-b - Math.sqrt(discriminant)) / (2 * a);
+
+        return TWO_SOLUTIONS + (Math.min(firstRoot, secondRoot)) + ";" + (Math.max(firstRoot, secondRoot));
     }
 }
